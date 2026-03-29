f72e012bae7852aaf23594d08c59c30b9d7807bf
Rachith H <157898134+Rachith-H@users.noreply.github.com>
2026-03-29 20:00:02 +0530

    Update gpio_testing.c

--- TASK - 2/RTL/gpio_testing.c
+++ TASK - 2/RTL/gpio_testing.c
@@ -1,13 +1,13 @@
-// Based on your One-Hot Address: Word Bit 3 -> 0x00400020
+// Based on One-Hot Address: Word Bit 3 -> 0x00400020
 #define GPIO_REG *((volatile unsigned int*)0x00400020)
 
 void _start(void) {
     // 1. Write a distinct pattern to the IP
-    // You should see this value appear in GTKWave on the GPIO_reg signal
+    // this value appears in GTKWave on the GPIO_reg signal
     GPIO_REG = 0xDEADE123;
 
     // 2. Read it back into a local variable
-    // This will trigger the 'assign IP_rdata' logic in your Verilog
+    // This will trigger the 'assign IP_rdata' logic in Verilog
     volatile unsigned int read_data;
     read_data = GPIO_REG;
 
